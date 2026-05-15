e5f3b631470f9a6ba205a0b392a0ec023599a275
Frank Batista <nybatista@gmail.com>
2026-05-15 14:54:09 -0400

    revised line break test to include br tags

--- src/app/internal/traits/spyne-cms-panel-data-property-traits.js
+++ src/app/internal/traits/spyne-cms-panel-data-property-traits.js
@@ -36,15 +36,15 @@ export class SpyneCmsPanelDataPropertyTraits extends SpyneTrait {
     let inputType = 'input';
     let rowsNum = 0;
     const shortTxtRE = /^((\d)+|true|false|(http(s)?.*)|\/\/.*|([a-zA-Z\w\s\S\W\\/]{0,34}))$/m;
-    const hasLineBreak = /[\r\n\u2028\u2029]/m;
+    const hasLineBreak = /[\r\n\u2028\u2029]|<br\s*\/?>/m;
 
     const type = UtilTraits.util$GetType(cmsVal);
     if (type!=='primitive'){
       return {inputType, rowsNum};
     }
 
-    const isInputTxt = shortTxtRE.test(cmsVal) && !hasLineBreak.test(cmsVal);
-    inputType = isInputTxt ? inputType : 'textarea';
+    const isInputTxt = shortTxtRE.test(cmsVal);
+    inputType = !isInputTxt || hasLineBreak.test(cmsVal) ? 'textarea' : inputType;
     rowsNum = isInputTxt ? rowsNum : Math.ceil(String(cmsVal).length / 33);
 
     return {inputType, rowsNum }
